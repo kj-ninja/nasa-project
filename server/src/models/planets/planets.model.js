690f03adac045550d4ee028ca762834aaa3fc53c
kjakubiak <rafalek3007>
2022-06-22 15:17:15 +0200

    indents fix

--- server/src/models/planets/planets.model.js
+++ server/src/models/planets/planets.model.js
@@ -7,8 +7,8 @@ const Planet = db.planets;
 
 const isPlanetHabitable = (planet) => {
   return planet['koi_disposition'] === 'CONFIRMED'
-  && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-  && planet['koi_prad'] < 1.6;
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
 };
 
 async function savePlanet(planet) {
@@ -32,23 +32,23 @@ async function findPlanetById(id) {
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(path.join(__dirname, '..', '..', '..', 'data', 'kepler_data.csv'))
-    .pipe(parse({
-      comment: '#',
-      columns: true,
-    }))
-    .on('data', (data) => {
-      if (isPlanetHabitable(data)) {
-        savePlanet(data);
-      }
-    })
-    .on('error', (error) => {
-      console.log(error);
-      reject(error);
-    })
-    .on('end', async () => {
-      console.log(`There are ${await Planet.count()} habitable planets!`);
-      resolve();
-    });
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isPlanetHabitable(data)) {
+          savePlanet(data);
+        }
+      })
+      .on('error', (error) => {
+        console.log(error);
+        reject(error);
+      })
+      .on('end', async () => {
+        console.log(`There are ${await Planet.count()} habitable planets!`);
+        resolve();
+      });
   });
 }
 
